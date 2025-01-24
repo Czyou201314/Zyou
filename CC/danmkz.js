3483773952816e0f7a4fae04f5451a5e408209d2
i <150835392+Czyou201314@users.noreply.github.com>
2025-01-24 18:22:07 +0800

    Add files via upload

---
+++ CC/danmkz.js
@@ -0,0 +1,12 @@
+    {
+    "danMu": [
+        {
+            "name": "个人弹幕",
+            "url": "./danm.js",
+            "codeID": "CNvXwjPskqxbJPPevV0vuOpfanjOXj7t",
+            "remark": "搜不到就算了吧",
+            "type": 400,
+            "version": 4
+        }
+    ]
+    }
