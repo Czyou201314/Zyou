3442b43f3e70f91efd521709d441d581d21c81a3
i <150835392+Czyou201314@users.noreply.github.com>
2025-01-24 18:44:12 +0800

    Update danmkz.js

--- CC/danmkz.js
+++ CC/danmkz.js
@@ -2,8 +2,8 @@
     "danMu": [
         {
             "name": "个人弹幕",
-            "url": "./danm.js",
-            "codeID": "CNvXwjPskqxbJPPevV0vuOpfanjOXj7t",
+            "url": "https://zyou.pages.dev/CC/danm.js",
+            "codeID": "BNvXwjPskqxbJPPevV0vuOpfanjOXj7t",
             "remark": "搜不到就算了吧",
             "type": 400,
             "version": 5
