622928e2e644f36ac92ce69a557c9be704ee5da9
i <150835392+Czyou201314@users.noreply.github.com>
2025-04-07 18:42:04 +0800

    Add files via upload

--- Tv/zyou.js
+++ Tv/zyou.js
@@ -1,15 +1,9 @@
 {
 "recommend": [
         {
-            "name": "豆瓣推荐 影视推荐",
-            "codeID": "N7WRmLFSh9M",
-            "url": "https://zyou.pages.dev/CC/douban.txt",
-            "version": "4",
-            "type": 200
-        },{
             "name": "豆瓣推荐2",
             "codeID": "N7WRmLmOdB8FSh4M",
-            "url": "http://xn--ihqu10cn4c.xn--v4q818bf34b.com/UZ/tools/Douban.txt",
+            "url": "https://zyou.pages.dev/Tv/db.txt",
             "version": "3",
             "type": 200
         }
@@ -26,131 +20,16 @@
         }
     ],
     "danMu": [
-        {
-            "name": "测试弹幕V5",
-            "url": "https://zyou.pages.dev/CC/danm.js",
-            "codeID": "CNvXwjPskqxbJPPevV0vuOpfanjOXj7t",
-            "remark": "搜了",
-            "type": 400,
-            "version": 5
-        },
-        {
-            "name": "测试弹幕V8",
-            "url": "https://zyou.pages.dev/CC/danmV8.js",
-            "codeID": "wjPskqxbJPPevV0vuOpft",
-            "remark": "搜不了",
-            "type": 400,
-            "version": 5
-        },
-        {
-            "name": "测试弹幕V9",
-            "url": "https://zyou.pages.dev/CC/danmuV9.js",
-            "codeID": "CNjPskqxbJPPeXj7t",
-            "remark": "算了",
-            "type": 400,
-            "version": 5
-        },
         
-        {
-            "name": "xin弹幕V7",
-            "url": "https://zyou.pages.dev/CC/弹幕源.js",
-            "codeID": "GrLpTPWlxvDXv5LsL6iOsY8AUZJWqE5x",
-            "remark": "别那么执着，搜不到就算了",
-            "type": 400,
-            "version": 7
-       } ,
-        
-        {
-            "name": "弹幕V10",
-            "url": "https://zyou.pages.dev/CC/dmV10.js",
-            "codeID": "GrLpTPWlxvDXvUZJWqE5x",
-            "remark": "别那么执着，搜不到就算了",
-            "type": 400,
-            "version": 7
-        },
         {
             "name": "弹幕V102",
-            "url": "https://zyou.pages.dev/CC/dmV102.js",
+            "url": "https://zyou.pages.dev/TV/dm10.js",
             "codeID": "GrLpTPWlxvDXv5LsL6iOsY8AUZJWqE5x",
             "remark": "别那么执着，搜不到就算了",
             "type": 400,
-            "version": 7
+            "version": "7"
         }
-    ],
-    "live": [
-        {
-            "name": "YanG",
-            "url": "https://tv.iill.top/m3u/Gather",
-            "type": 10,
-            "remark": "来自 YanG-1989 大佬 https://github.com/YanG-1989"
-        }
-    ],
-    "vod": [
-        {
-            "name": "玩偶",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/wogg.js",
-            "webSite": "https://www.wogg.net",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "3",
-            "type": 101
-        },
-        {
-            "name": "木偶",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/mogg.js",
-            "webSite": "http://www.muoupan.top",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "2",
-            "type": 101
-        },
-        {
-            "name": "小米",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/xiaomi.js",
-            "webSite": "http://www.mucpan.cc",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "3",
-            "type": 101
-        },
-        {
-            "name": "蜡笔",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/labipan.js",
-            "webSite": "https://duopan.fun",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "3",
-            "type": 101
-        },
-        {
-            "name": "至臻",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/zhizhen.js",
-            "webSite": "https://mihdr.top",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "3",
-            "type": 101
-        },
-        {
-            "name": "多多",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/duoduo.js",
-            "webSite": "https://tv.yydsys.top",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "3",
-            "type": 101
-        },
-        {
-            "name": "欧歌",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/ouge.js",
-            "webSite": "https://woog.nxog.eu.org",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": "3",
-            "type": 101
-        },
-        {
-            "name": "云盘",
-            "codeID": "",
-            "api": "https://gitee.com/woleigedouer/uzVideo-extensions/raw/main/vod/js/resyunpan.js",
-            "instance": "resYunPan20241120",
-            "webSite": "https://res.yunpan.win",
-            "remark": "𝓓𝓸𝓾'𝓮𝓻",
-            "version": 1,
-            "type": 100
-        }
+    
+
     ]
 }
